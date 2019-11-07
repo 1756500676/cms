c6b7d6f05918a5a8f04044992af6d38b9f483c12
1756500676 <1756500676@qq.com>
2019-11-07 10:31:47 +0800

    第一次提交

---
+++ src/main/java/com/sw/test/Demo01.java
@@ -0,0 +1,27 @@
+/**  
+ * zsw 公司 ZhenShuaiWei_SSM_PROJECT
+ * @Title: Demo01.java 
+ * @Prject: zhenshuaiwei-day1107exam-git
+ * @Package: com.sw.test 
+ * @Description: TODO
+ * @author: zsw  
+ * @date: 2019年11月7日 上午8:10:43 
+ * @version: V1.0   
+ */
+package com.sw.test;
+
+/** 
+ * @ClassName: Demo01 
+ * @Description: TODO
+ * @author:zsw 
+ * @date: 2019年11月7日 上午8:10:43  
+ */
+public class Demo01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java!~");
+		
+		
+	}
+}
